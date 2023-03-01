4825d475c72d6f9d01473b2a3d1ca7071d580df1
0lgaIvanova <olgadavidenkova@inbox.lv>
2023-03-01 09:45:15 +0200

    User class.

---
+++ team_fitness_app/src/main/java/User.java
@@ -0,0 +1,45 @@
+import java.util.Objects;
+
+public class User {
+
+    private String username;
+    private String password;
+    private static int lastId = 0;
+    private int id;
+
+    public User(String username, String password, int lastId, int id) {
+        this.username = username;
+        this.password = password;
+        this.id = ++lastId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password, id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
