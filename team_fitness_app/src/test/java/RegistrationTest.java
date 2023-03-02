02e5694f172726ada327ab4e299a64b39b2bf7ec
arsenijsOvcinkins <85284832+arsenijsOvcinkins@users.noreply.github.com>
2023-03-02 04:52:19 +0200

    Update RegistrationTest.java

--- team_fitness_app/src/test/java/RegistrationTest.java
+++ team_fitness_app/src/test/java/RegistrationTest.java
@@ -1,5 +1,9 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegistrationTest {
