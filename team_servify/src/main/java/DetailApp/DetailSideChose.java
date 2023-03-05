38e416595c4521e92f390f940203f5fa6d2c9036
olegsradigin <oradigins@gmail.com>
2023-03-05 12:23:24 +0200

    correction

--- team_servify/src/main/java/DetailApp/DetailSideChose.java
+++ team_servify/src/main/java/DetailApp/DetailSideChose.java
@@ -9,10 +9,10 @@ public class DetailSideChose {
         System.out.println("Enter detail side: \n" +
                 "1.Left\n" +
                 "2.Right");
-        int sideChose = Integer.parseInt(scanner.nextLine());
-        if (sideChose == 1) {
+        int sideChoise = Integer.parseInt(scanner.nextLine());
+        if (sideChoise == 1) {
             newDetailSide = "Left";
-        } else if (sideChose == 2) {
+        } else if (sideChoise == 2) {
             newDetailSide = "Right";
         }
         return newDetailSide;
