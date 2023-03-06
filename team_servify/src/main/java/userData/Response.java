acfac02bff253de22726ba2ae3acb4c5191754ab
lblinnikovs <levs.blinnikovs@gmail.com>
2023-03-06 21:44:09 +0200

    added some questions

--- team_servify/src/main/java/userData/Response.java
+++ team_servify/src/main/java/userData/Response.java
@@ -4,8 +4,8 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 public class Response {
-    private boolean isApproved;
-    private Date date;
-    private BigDecimal price;
-    private String notes;
+    private boolean isApproved; // maybe this all can be put to 'Request'?
+    private Date date; // maybe this all can be put to 'Request'?
+    private BigDecimal price; // maybe this all can be put to 'Request'?
+    private String notes; // maybe this all can be put to 'Request'?
 }
