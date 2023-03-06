0e1e1b75e7da2ee5e465d181cefa8f3958ae70bc
lblinnikovs <levs.blinnikovs@gmail.com>
2023-03-06 13:19:43 +0200

    Fix - do not offer detail choice to delete when empty detail list

--- team_servify/src/main/java/DetailApp/DetailListApplication.java
+++ team_servify/src/main/java/DetailApp/DetailListApplication.java
@@ -32,8 +32,12 @@ class DetailListApplication {
                     System.out.println("Your detail was added to list.");
                 }
                 case 2 -> {
-                    app.details.remove(app.detailChoice.detailChoice());
-                    System.out.println("Your detail was removed from list.");
+                    if (app.details.isEmpty()) {
+                        System.out.println("You don't have any detail to remove!");
+                    } else {
+                        app.details.remove(app.detailChoice.detailChoice());
+                        System.out.println("Your detail was removed from list.");
+                    }
                 }
                 case 3 -> {
                     System.out.println("Detail list: ");
