20f3bd276633d26ab4226ef491a89e2b1137631f
Supesuba <asura122@gmail.com>
2023-03-13 22:52:54 +0200

    Add new class Product, created Interface and a Manager App.

---
+++ team_marketplace/src/main/java/product/Product.java
@@ -0,0 +1,59 @@
+package product;
+
+public class Product implements ProductInterface {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+
+    public Product(int id, String name, String description, double price, String category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+
+    }
+
+ //   Getters and Setters required ?
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
