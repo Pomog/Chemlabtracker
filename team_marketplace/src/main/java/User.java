2d9b218f6fb80b43525967df8334b9147e657793
olegsradigins <oradigins@gmail.com>
2023-03-14 21:21:12 +0200

    correction

--- team_marketplace/src/main/java/User.java
+++ team_marketplace/src/main/java/User.java
@@ -5,7 +5,7 @@ public class User {
     private String name;
     private String email;
 
-    public User (String username, String password, String name, String email){
+    public User (String username, String password){
         this.username = username;
         this.password = password;
         this.name = name;
