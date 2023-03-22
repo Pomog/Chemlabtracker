33ad9e0e02e98b642a6099ce159b1923ee0d33a7
lFabrio <azureon94@gmail.com>
2023-03-22 22:48:59 +0200

    order rename

--- team_servify/src/main/java/lv/javaguru/java2/servify/domain/Order.java
+++ team_servify/src/main/java/lv/javaguru/java2/servify/domain/Order.java
@@ -1,21 +1,20 @@
 package lv.javaguru.java2.servify.domain;
 
-import lv.javaguru.java2.servify.domain.RequestStatus;
 import lv.javaguru.java2.servify.rezerv.Detail;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
-public class Request {
+public class Order {
     private Long userId; // so user id or client id? Only client makes requests. Manager just approves...
     private Detail detail; // need to make it a list
-    private Date requestDate;
+    private Date orderDate;
     private Date systemResponseDate;
     private Date managerResponseDate; // do we need this? Or just one final interaction date?
     private BigDecimal priceInitial; // gen by system
     private BigDecimal priceManager; // overriden by manager
     private BigDecimal priceFinal;
-    private RequestStatus requestStatus;
+    private OrderStatus orderStatus;
     private BigDecimal price;
     private String notes;
 }
