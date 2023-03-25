5960287fdf57625a68c3a9549393f97a8d880633
arthurkov98 <30414869+arthurkov98@users.noreply.github.com>
2023-03-25 22:01:22 +0300

    add ListShopItemsServiceTest

--- team_planet_express/src/test/java/core/services/actions/customer/ListShopItemsServiceTest.java
+++ team_planet_express/src/test/java/core/services/actions/customer/ListShopItemsServiceTest.java
@@ -1,7 +1,25 @@
 package core.services.actions.customer;
 
+import core.database.Database;
+import core.requests.customer.ListShopItemsRequest;
+import core.responses.customer.ListShopItemsResponse;
+import core.services.fake.FakeDatabaseInitializer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
 class ListShopItemsServiceTest {
+    private final Database fakeDatabase = new Database();
+    private final ListShopItemsRequest mockRequest = mock(ListShopItemsRequest.class);
 
-    //TODO test
+    private final ListShopItemsService service =
+            new ListShopItemsService(fakeDatabase);
 
+    @Test
+    void shouldListHeaderAndItem() {
+        new FakeDatabaseInitializer(fakeDatabase).initialize();
+        ListShopItemsResponse response = service.execute(mockRequest);
+        assertEquals(10, response.getShopItems().size());
+    }
 }
