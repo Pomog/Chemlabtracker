2e9dc97394039662d546009a044cf4137f4a8509
0lgaIvanova <olgadavidenkova@inbox.lv>
2023-03-29 15:12:10 +0300

    Lesson 4 corrections (renamed from Validate to Login).

--- team_fitness_app/src/main/java/java2/fitness_app/users/users/core/requests/LoginUserRequest.java
+++ team_fitness_app/src/main/java/java2/fitness_app/users/users/core/requests/LoginUserRequest.java
@@ -1,11 +1,11 @@
 package java2.fitness_app.users.users.core.requests;
 
-public class ValidateUserRequest {
+public class LoginUserRequest {
 
     private Long userIdToLogin;
     private String password;
 
-    public ValidateUserRequest(Long userIdToLogin, String password) {
+    public LoginUserRequest(Long userIdToLogin, String password) {
         this.userIdToLogin = userIdToLogin;
         this.password = password;
     }
