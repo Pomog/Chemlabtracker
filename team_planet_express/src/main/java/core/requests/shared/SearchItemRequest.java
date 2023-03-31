343f3dbee97cfcfe9837d0204da6c7561f1e377d
arthurkov98 <30414869+arthurkov98@users.noreply.github.com>
2023-03-31 16:40:22 +0300

    add ordering to search

--- team_planet_express/src/main/java/core/requests/shared/SearchItemRequest.java
+++ team_planet_express/src/main/java/core/requests/shared/SearchItemRequest.java
@@ -2,8 +2,11 @@ package core.requests.shared;
 
 import lombok.Value;
 
+import java.util.List;
+
 @Value
 public class SearchItemRequest {
     String itemName;
     String price;
+    List<Ordering> orderings;
 }
--- team_planet_express/src/main/java/core/services/actions/shared/SearchItemService.java
+++ team_planet_express/src/main/java/core/services/actions/shared/SearchItemService.java
@@ -2,6 +2,7 @@ package core.services.actions.shared;
 
 import core.database.Database;
 import core.domain.item.Item;
+import core.requests.shared.Ordering;
 import core.requests.shared.SearchItemRequest;
 import core.responses.CoreError;
 import core.responses.shared.SearchItemResponse;
@@ -9,7 +10,10 @@ import core.services.validators.shared.SearchItemValidator;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class SearchItemService {
     private final Database database;
@@ -22,27 +26,55 @@ public class SearchItemService {
 
     public SearchItemResponse execute(SearchItemRequest request) {
         List<CoreError> errors = validator.validate(request);
+        SearchItemResponse response;
         if (!errors.isEmpty()) {
-            //null? o.0
-            return new SearchItemResponse(null, errors);
+            response = new SearchItemResponse(null, errors);
+        } else {
+            List<Item> items = search(request);
+            items = order(items, request.getOrderings());
+            response = new SearchItemResponse(items, null);
         }
+        // only one return POG
+        return response;
+    }
+
+    private List<Item> search(SearchItemRequest request) {
         List<Item> items;
-        //Those are all wrong
-        //That's what tests are for..
-        //It just dies with a NumberFormatException when there is no price
-        //Why no request.getItemName() != null ? current one looks weird and unnecessary convoluted
-        //it also allows for blank name
-        if (!(request.getItemName() == null) && isPresent(request.getPrice())) {
+
+        if (request.getItemName() != null && !isPresent(request.getPrice())) {
             items = database.accessItemDatabase().searchByName(request.getItemName());
-        } else if (!(request.getItemName() == null) && !isPresent(request.getPrice())) {
+        } else if (request.getItemName() != null && isPresent(request.getPrice())) {
             BigDecimal price = new BigDecimal(request.getPrice()).setScale(2, RoundingMode.HALF_UP);
             items = database.accessItemDatabase().searchByNameAndPrice(request.getItemName(), price);
         } else {
             items = database.accessItemDatabase().getAllItems();
         }
-        //why null?
-        //we do not pass null in any other service..
-        return new SearchItemResponse(items, null);
+        return items;
+    }
+
+    private List<Item> order(List<Item> items, List<Ordering> orderings) {
+        if (orderings != null) {
+            Stream<Item> itemStream = items.stream();
+            Optional<Ordering> orderingByPriceOptional = orderings.stream().
+                    filter(ordering -> ordering.getOrderBy().equals("price")).findFirst();
+            if (orderingByPriceOptional.isPresent()) {
+                itemStream = itemStream.sorted(
+                        orderingByPriceOptional.get().getOrderDirection().equals("reverse") ?
+                                Comparator.comparing(Item::getPrice).reversed() :
+                                Comparator.comparing(Item::getPrice)
+                );
+            }
+            Optional<Ordering> orderingByItemsOptional = orderings.stream().filter(ordering -> ordering.getOrderBy().equals("item")).findFirst();
+            if (orderingByItemsOptional.isPresent()) {
+                itemStream = itemStream.sorted(
+                        orderingByItemsOptional.get().getOrderDirection().equals("reverse") ?
+                                Comparator.comparing(Item::getName).reversed() :
+                                Comparator.comparing(Item::getName)
+                );
+            }
+            items = itemStream.toList();
+        }
+        return items;
     }
 
     private boolean isPresent(String value) {
