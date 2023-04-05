fbe5882cde216fe7a10ca4c940d6882eded489fa
arsenijsOvcinkins <arsenijs.ovcinkins@gmail.com>
2023-04-05 06:02:45 +0300

    ApplicationContext class added and plugged into UserListApplication

--- team_fitness_app/src/main/java/java2/fitness_app/users/UserListApplication.java
+++ team_fitness_app/src/main/java/java2/fitness_app/users/UserListApplication.java
@@ -10,23 +10,7 @@ import java.util.Scanner;
 
 public class UserListApplication {
 
-    private static Database database = new InMemoryDatabaseImpl();
-    private static AddUserRequestValidator addUserRequestValidator = new AddUserRequestValidator();
-    private static LoginUserValidator loginUserValidator = new LoginUserValidator();
-    private static AddUserService addUserService = new AddUserService(database, addUserRequestValidator);
-    private static LoginUserService loginUserService = new LoginUserService(database, loginUserValidator);
-    private static RemoveUserValidator removeUserValidator = new RemoveUserValidator();
-    private static RemoveUserService removeUserService = new RemoveUserService(database, removeUserValidator);
-    private static GetAllUsersService getUsersService = new GetAllUsersService(database);
-
-
-    private static UIAction addUserUIAction = new AddUserUIAction(addUserService);
-    private static UIAction loginUserUIAction = new LoginUserUIAction(loginUserService);
-    private static UIAction removeUserUIAction = new RemoveUserUIAction(removeUserService);
-    private static UIAction getAllUsersUIAction = new GetAllUsersUIAction(getUsersService);
-
-
-    private static UIAction exitUIAction = new ExitUIAction();
+   private static ApplicationContext applicationContext = new ApplicationContext();
 
 
     public static void main(String[] args) {
@@ -58,11 +42,26 @@ public class UserListApplication {
     private static void executeSelectedMenuItem(int selectedMenu) {
 
         switch (selectedMenu) {
-            case 1 -> addUserUIAction.execute();
-            case 2 -> loginUserUIAction.execute();
-            case 3 -> removeUserUIAction.execute();
-            case 4 -> getAllUsersUIAction.execute();
-            case 5 -> exitUIAction.execute();
+            case 1 -> {
+                AddUserUIAction uiAction = applicationContext.getBean(AddUserUIAction.class);
+                uiAction.execute();
+            }
+            case 2 -> {
+                LoginUserUIAction uiAction = applicationContext.getBean(LoginUserUIAction.class);
+                uiAction.execute();
+            }
+            case 3 -> {
+                RemoveUserUIAction uiAction = applicationContext.getBean(RemoveUserUIAction.class);
+                uiAction.execute();
+            }
+            case 4 -> {
+                GetAllUsersUIAction uiAction = applicationContext.getBean(GetAllUsersUIAction.class);
+                uiAction.execute();
+            }
+            case 5 -> {
+                ExitUIAction uiAction = applicationContext.getBean(ExitUIAction.class);
+                uiAction.execute();
+            }
         }
     }
 }
