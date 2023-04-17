84c2628e886b0fde993e821169450622ad23142e
arthurkov98 <30414869+arthurkov98@users.noreply.github.com>
2023-04-17 15:33:50 +0300

    remake acceptance test v2

--- team_planet_express/src/test/java/shop/acceptance/custom/CustomAcceptanceTest.java
+++ team_planet_express/src/test/java/shop/acceptance/custom/CustomAcceptanceTest.java
@@ -2,49 +2,31 @@ package shop.acceptance.custom;
 
 import shop.ApplicationContext;
 import shop.acceptance.ApplicationContextSetup;
-import shop.core.database.Database;
-import shop.core.domain.cart.Cart;
-import shop.core.domain.cart.CartStatus;
-import shop.core.responses.customer.ListCartItemsResponse;
-import shop.core.responses.customer.ListShopItemsResponse;
-import shop.core.support.CurrentUserId;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import shop.acceptance.custom.tester.*;
 
 public abstract class CustomAcceptanceTest {
     private final ApplicationContextSetup applicationContextSetup = new ApplicationContextSetup();
     private final ApplicationContext applicationContext = applicationContextSetup.setupApplicationContext();
 
-    final ServiceRequest serviceRequest = new ServiceRequest(applicationContext);
-    final ServiceChecker serviceChecker = new ServiceChecker(applicationContext);
 
-    protected void checkItemInListShopItems(String itemName, int quantity) {
-        ListShopItemsResponse listShopItemsResponse = serviceRequest.listShopItems();
-        serviceChecker.checkItemInListShop(listShopItemsResponse, itemName, quantity);
+    protected AddItemToCartTester addItemToCart() {
+        return new AddItemToCartTester(applicationContext);
     }
 
-    protected void addAndCheckItemInCart(String itemName, Integer buyQuantity, Integer shopQuantity) {
-        serviceRequest.addItemToCart(itemName, buyQuantity.toString());
-        serviceChecker.compareCartItem(itemName, buyQuantity);
-        checkItemInListShopItems(itemName, shopQuantity - buyQuantity);
+    protected ListShopItemsTester listShopItems() {
+        return new ListShopItemsTester(applicationContext);
     }
 
-    protected void checkItemInListCartItems(String itemName, Integer quantity) {
-        ListCartItemsResponse listCartItemsResponse = serviceRequest.listCartItems();
-        serviceChecker.checkItemInCart(listCartItemsResponse, itemName, quantity);
+    protected RemoveItemFromCartTester removeItemFromCart() {
+        return new RemoveItemFromCartTester(applicationContext);
     }
 
-    protected void checkRemoveItemFromCart(String itemName) {
-        serviceRequest.removeItemFromCart(itemName);
-        serviceChecker.NotItemInCart(itemName);
+    protected ListCartItemsTester listCartItems() {
+        return new ListCartItemsTester(applicationContext);
     }
 
-    protected void checkBuyCart() {
-        Database database = applicationContext.getBean(Database.class);
-        CurrentUserId currentUserId = applicationContext.getBean(CurrentUserId.class);
-        Cart currentCart = database.accessCartDatabase().findOpenCartForUserId(currentUserId.getValue()).get();
-        serviceRequest.buyCart();
-        assertEquals(currentCart.getCartStatus(), CartStatus.CLOSED);
+    protected BuyTester buyCart() {
+        return new BuyTester(applicationContext);
     }
 
 }
