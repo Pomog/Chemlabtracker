e58df11c77ffdd633bec8f4723a27d6d0234e95c
0lgaIvanova <olgadavidenkova@inbox.lv>
2023-04-18 13:22:26 +0300

    Add Ordering and Paging.

--- team_fitness_app/src/main/java/java2/fitness_app/exercises/core/exercise_requests/SearchExercisesRequest.java
+++ team_fitness_app/src/main/java/java2/fitness_app/exercises/core/exercise_requests/SearchExercisesRequest.java
@@ -5,4 +5,16 @@ public class SearchExercisesRequest {
     private Ordering ordering;
     private Paging paging;
 
+    public SearchExercisesRequest(Ordering ordering, Paging paging) {
+        this.ordering = ordering;
+        this.paging = paging;
+    }
+
+    public Ordering getOrdering() {
+        return ordering;
+    }
+
+    public Paging getPaging() {
+        return paging;
+    }
 }
