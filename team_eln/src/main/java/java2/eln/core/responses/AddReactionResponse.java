32826e1ae8a64851ee9825fc121039c533327787
Pomog <thoryur@gmail.com>
2023-04-27 00:23:46 +0300

    team_eln. DatabaseIM, sortReactions and Documentation Comments

--- team_eln/src/main/java/java2/eln/core/responses/AddReactionResponse.java
+++ team_eln/src/main/java/java2/eln/core/responses/AddReactionResponse.java
@@ -6,18 +6,40 @@ import java2.eln.domain.ReactionData;
 
 import java.util.List;
 
+/**
+ * A response object that encapsulates the result of adding a reaction to the system.
+ */
 public class AddReactionResponse extends CoreResponse {
+    /**
+     * The reaction data that was added to the system.
+     */
     private ReactionData reactionData;
 
+    /**
+     * Creates a new AddReactionResponse object with a list of errors.
+     *
+     * @param errors the list of errors to include in the response
+     */
     public AddReactionResponse(List<CoreError> errors) {
         super(errors);
     }
 
+    /**
+     * Creates a new AddReactionResponse object with the specified reaction data.
+     *
+     * @param reactionData the reaction data to include in the response
+     */
     public AddReactionResponse(ReactionData reactionData) {
         this.reactionData = reactionData;
     }
 
+    /**
+     * Gets the reaction data included in the response.
+     *
+     * @return the reaction data
+     */
     public ReactionData getReactionData() {
         return reactionData;
     }
 }
+
